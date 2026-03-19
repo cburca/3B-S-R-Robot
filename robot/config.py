7ff9846cab516e517e4bfb61f26eb31e6c5c0594
Hunter Adams <hmadams@uwaterloo.ca>
2026-03-19 15:35:12 -0400

    cleaning up config.py, removing defunct variables

--- robot/config.py
+++ robot/config.py
@@ -1,47 +1,29 @@
 class Config:
+    # Camera
     CAM_INDEX = 0
     CAM_W = 640
     CAM_H = 480
     CAM_FPS = 30.0
 
-    SERIAL_PORT = "/dev/ttyACM0"
+    # Serial Communication
     BAUD = 115200
     SERIAL_TIMEOUT = 0.1
     SERIAL_HANDSHAKE = True
-    VISION_W = 320
-    VISION_H = 240
-
     SERIAL_WAIT_TIMEOUT = 15.0
-    SERIAL_CONNECT_RETRIES = 10
-    SERIAL_RETRY_DELAY = 1.0
-    ARDUINO_RESET_DELAY = 2.0
+    SERIAL_PORT = 'dev/ttyACM0' # port on Raspberry PI, fwt and I'll cut you
 
-    CAM_CONNECT_RETRIES = 10
-    CAM_RETRY_DELAY = 1.0
-
+    # Main Loop
     MAIN_RETRY_DELAY = 2.0
-
     LOOP_HZ = 20.0
-    RUN_TIME_S = 10.0
+    RUN_TIME_S = 20.0
 
-    KP_HEADING = 0.0
-    KD_HEADING = 0.0
-    YAW_RATE_MAX = 2.0
-
-    V_MAX = 0.25
-    V_MIN = 0.0
-    KV = 0.25
-
-    WHEEL_RADIUS_M = 0.03
-    TRACK_WIDTH_M = 0.14
-    ENC_CPR = 2797
-    MAX_TPS = 2000.0
-
+    # Color Thresholds
     RED_LOWER1 = (0, 100, 100)
     RED_UPPER1 = (10, 255, 255)
     RED_LOWER2 = (160, 100, 100)
     RED_UPPER2 = (180, 255, 255)
 
+    # Image Processing
     MIN_MASK_AREA = 300
     CANNY1, CANNY2 = 50, 150
     HOUGH_THRESH = 30
@@ -51,38 +33,36 @@ class Config:
     YREF_FRAC = 0.85
     MAX_ABS_DEG_FROM_VERTICAL = 60.0
 
-    DEBUG_SHOW = True
-    DEBUG_DRAW = True
+    # Debug
+    DEBUG_SHOW = False
+    DEBUG_DRAW = False
 
-    # Robot Geometry - TO BE UPDATED
+    # Robot Geometry
     r = 0.04
     L = 0.20
     vmax = 0.2
-    wmax = vmax / r  # 6.25 rad/s 
+    wmax = vmax / r  # 6.25 rad/s is based off latency constraint, not firm
 
-    # Timing Rates
+    # Encoder
+    ENCODER_CPR = 2797
+
+    # Timing
     DT_OUTER = 1.0 / CAM_FPS # ~0.033s
     INNER_HZ = 150.0
     DT_INNER = 1.0 / INNER_HZ
     MAX_RUN_S = 10.0
     LINE_LOST_TIMEOUT = 1.5
+
+    # Control - Velocity
     V_SLEW_UP = 0.25 
     V_SLEW_DOWN = 0.75 
+    V_MIN = 0.01
+    KV = 0.0 # Speed reduction factor when turning, might remove
+
+    # Control - Yaw
     YAW_SLEW = 0.0
-    WHEEL_OMEGA_LIMIT = 10.0
-    
+    WHEEL_OMEGA_LIMIT = 10
     OFFSET_TO_ANGLE_GAIN = 0.001
-    # Outer PD Gains - TO BE UPDATED
     KP_THETA = 2.6
     KD_THETA = 0.45
-    U_YAW_LIMIT = 1.0 # normalized yaw actuation command
-
-    KV = 0.0  # TO BE TUNED: v_cmd = vmax (1 - KV*|yaw_cmd|)
-    V_MIN = 0.01
-
-    # encoder counts per revolution
-    ENCODER_CPR = 2797
-
-    # USB Serial Params
-    SERIAL_PORT = 'dev/ttyACM0'  # update when applicable
-    BAUD_RATE = 115200 # update to real value
+    U_YAW_LIMIT = 1.0
