105507e02688adefaf15309a5babed465be9278d
Hunter Adams <hmadams@uwaterloo.ca>
2026-03-27 01:21:11 -0400

    manuever changes

--- Linefollower/include/maneuvers.h
+++ Linefollower/include/maneuvers.h
@@ -3,6 +3,9 @@
 #include <Arduino.h>
 #include "motors.h"
 #include "encoders.h"
+#include "config.h"
 
-bool turnDegrees(float deg, Motors& motors, Encoders& encoders);
-bool dropOff(float extra_offset, Motors& motors, Encoders& encoders);
+// static const int SAMPLE_TIMEOUT_MS = 1000; // DONT THINK WE NEED or USE 
+
+void turnDegrees(float turn_deg, Motors& motors, Encoders& encoders);
+void dropOff(Motors& motors, Encoders& encoders);
