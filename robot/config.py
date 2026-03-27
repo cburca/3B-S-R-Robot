916319357a13bb611f2c7e3b1d66d0e0e8623002
nathanielli <li.nathaniel8@gmail.com>
2026-03-27 00:43:50 -0400

    updated HSV for red

--- robot/config.py
+++ robot/config.py
@@ -17,9 +17,9 @@ class Config:
     RUN_TIME_S = 20.0
 
     # Color Thresholds
-    RED_LOWER1 = (0, 90, 90)
+    RED_LOWER1 = (0, 90, 100)
     RED_UPPER1 = (20, 255, 255)
-    RED_LOWER2 = (160, 90, 90)
+    RED_LOWER2 = (160, 22, 75)
     RED_UPPER2 = (180, 255, 255)
     BLUE_LOWER = (100, 90, 80)
     BLUE_UPPER = (120, 255, 255)
